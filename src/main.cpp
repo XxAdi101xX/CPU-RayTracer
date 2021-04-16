0ed7fb0abee1764e10e6c27593933f7d76db811c
Adithya Venkatarao <adi_101@live.com>
2021-04-15 23:13:32 -0400

    Implement ray-sphere intersection test and render a sphere

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,23 @@
 
 #include <iostream>
 
+bool hit_sphere(const point3 &center, double radius, const ray &r) {
+    vec3 oc = r.origin() - center;
+    double a = dot(r.direction(), r.direction());
+    double b = 2.0 * dot(oc, r.direction());
+    double c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant > 0);
+}
+
+// conducting a lerp (linear interpolation) between white and blue
 color ray_color(const ray& r) {
+    if (hit_sphere(point3(0.0,0.0,-1.0), 0.5, r)) {
+        return color(1.0, 0.0, 0.0);
+    }
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
 int main() {
@@ -28,14 +41,12 @@ int main() {
 
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
-    for (int i = image_height - 1; i >= 0; --i) {
-        std::cerr << "\rScanlines remaining: " << i << ' ' << std::flush;
-        for (int j = 0; j < image_width; ++j) {
-            double u = double(j) / (image_width - 1);
-            double v = double(i) / (image_width - 1);
-
-            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-
+    for (int row = image_height-1; row >= 0; --row) {
+        std::cerr << "\rScanlines remaining: " << row << ' ' << std::flush;
+        for (int column = 0; column < image_width; ++column) {
+            double u = double(column) / (image_width-1);
+            double v = double(row) / (image_height-1);
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
             color pixel_color = ray_color(r);
             write_color(std::cout, pixel_color);
         }
