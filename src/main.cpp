5e0c2367a4d34508a773347fcbad525561311840
Adithya Venkatarao <adi_101@live.com>
2021-04-24 11:57:21 -0400

    Implement a texture class to represent surface color in place of raw color values

--- src/main.cpp
+++ src/main.cpp
@@ -33,11 +33,13 @@ color ray_color(const ray& r, const hittable& world, int depth) {
 hittable_list random_scene() {
     hittable_list world;
 
-    auto ground_material = std::make_shared<lambertian>(color(0.5, 0.5, 0.5));
-    world.add(std::make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
+    // auto ground_material = std::make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    // world.add(std::make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
+    auto checker = std::make_shared<checker_texture>(color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
+    world.add(std::make_shared<sphere>(point3(0,-1000,0), 1000, std::make_shared<lambertian>(checker)));
 
-    for (int a = -11; a < 11; a++) {
-        for (int b = -11; b < 11; b++) {
+    for (int a = -4; a < 4; a++) {
+        for (int b = -4; b < 4; b++) {
             auto choose_mat = random_double();
             point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
 
--- src/material.h
+++ src/material.h
@@ -2,6 +2,7 @@
 
 #include "ray.h"
 #include "hittable.h"
+#include "texture.h"
 
 struct hit_record;
 
@@ -14,7 +15,8 @@ class material {
 
 class lambertian : public material {
     public:
-        lambertian(const color& a) : albedo(a) {}
+        lambertian(const color& a) : albedo(std::make_shared<solid_color>(a)) {}
+        lambertian(std::shared_ptr<texture> a) : albedo(a) {}
 
         virtual bool scatter(
             const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered
@@ -27,12 +29,12 @@ class lambertian : public material {
                 scatter_direction = rec.normal;
             }
             scattered = ray(rec.p, scatter_direction, r_in.time());
-            attenuation = albedo;
+            attenuation = albedo->value(rec.u, rec.v, rec.p);;
             return true;
         }
 
     private:
-        color albedo;
+        std::shared_ptr<texture> albedo;
 };
 
 class metal : public material {
